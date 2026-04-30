091457bfc0e63c993e8e97ead2852be12cf1eabf
Jose Torres <jtorres19@unomaha.edu>
2026-04-29 22:21:34 -0500

    Update entrypoint.sh

--- .github/actions/terraform/entrypoint.sh
+++ .github/actions/terraform/entrypoint.sh
@@ -10,6 +10,10 @@ export STATE_KEY=${INPUT_STATE_KEY}
 export TF_STAGE=${INPUT_TF_STAGE}
 export DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}
 
+export TF_VAR_ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}
+export TF_VAR_ARM_CLIENT_SECRET=${INPUT_ARM_CLIENT_SECRET}
+export TF_VAR_DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}
+
 if [[ "$TF_STAGE" == "stage1" ]]; then
   terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
   terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
